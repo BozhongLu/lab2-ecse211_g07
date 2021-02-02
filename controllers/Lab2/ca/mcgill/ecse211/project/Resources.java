c214fcbcf7140a5e8dfb1bb988ea67edce51c186
BozhongLu <35471518+BozhongLu@users.noreply.github.com>
2021-02-02 02:30:56 -0500

    Change variables

--- controllers/Lab2/ca/mcgill/ecse211/project/Resources.java
+++ controllers/Lab2/ca/mcgill/ecse211/project/Resources.java
@@ -25,13 +25,13 @@ public class Resources {
   public static final double WHEEL_RAD = 0.027;
   
   /** The robot width in meters. */
-  public static final double BASE_WIDTH = 0;
+  public static final double BASE_WIDTH = 0.14;
   
   /** The speed at which the robot moves forward in degrees per second. */
-  public static final int FORWARD_SPEED = 0;
+  public static final int FORWARD_SPEED = 100;
   
   /** The speed at which the robot rotates in degrees per second. */
-  public static final int ROTATE_SPEED = 0;
+  public static final int ROTATE_SPEED = 75;
   
   /** The motor acceleration in degrees per second squared. */
   public static final int ACCELERATION = 3000;
